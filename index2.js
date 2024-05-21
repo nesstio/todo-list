35bb820635b2fdd210c77b0e008941ad0c795d3e
nesstio <prykhodko.anastasija@gmail.com>
2024-05-21 10:53:26 +0300

    breaking down functions (there is a bug somewhere! handle edit function)

--- index2.js
+++ index2.js
@@ -19,48 +19,40 @@ class Task{
         this.editButton.textContent = "edit";
 
     }
-    // #helpFunction(){
-    //     const inputElement = document.getElementById("input-text");
-    //     const inputedText = inputElement.value.trim();
-    //     this.inputValue = inputedText;
+    
+    #handleChangedTask(){
+        const inputElement = document.getElementById("input-text");
+        const inputedText = inputElement.value.trim();
+        this.inputValue = inputedText;
 
-    //     const el = document.getElementById(`divID-${this.idOfcurrentElement}`);
-    //     console.log(`divID-${this.idOfcurrentElement}`);
-    //     console.log(this);
-    //     el.firstElementChild.textContent = inputedText;
+        const el = document.getElementById(`divID-${this.idOfcurrentElement}`);
+        el.firstElementChild.textContent = inputedText;
 
-    //     addTaskButton.removeEventListener()
-        
-    //     console.log(this);
-    // }
+        addTaskButton.removeEventListener("click", () => this.#handleChangedTask());
+
+        addTaskButton.addEventListener("click", () => addNewTask);
+
+        addTaskButton.style.background = "green";
+        addTaskButton.value = "Add task";
+
+        inputElement.value = "";
+    }
+    #handleDeleteTask(){
+        document.getElementById(`divID-${this.idOfcurrentElement}`).remove();
+    }
+    #handleEditTask(){
+        document.getElementById("input-text").value = this.inputValue;
+            
+        addTaskButton.removeEventListener("click", addNewTask);
+        addTaskButton.style.background = "yellow";
+        addTaskButton.value = "change";
+
+        addTaskButton.addEventListener("click", () => this.#handleChangedTask());
+    }
     attachEventListeners(){
-        this.deleteButton.addEventListener("click", () => {
-            document.getElementById(`divID-${this.idOfcurrentElement}`).remove();
-        });
+        this.deleteButton.addEventListener("click", () => this.#handleDeleteTask());
 
-        this.editButton.addEventListener("click", () => {
-            document.getElementById("input-text").value = this.inputValue;
-            
-            addTaskButton.removeEventListener("click", addNewTask);
-            addTaskButton.style.background = "yellow";
-            console.log(addTaskButton);
-            addTaskButton.value = "change";
-
-            
-            addTaskButton.addEventListener("click", ()=>{
-                const inputElement = document.getElementById("input-text");
-                const inputedText = inputElement.value.trim();
-                this.inputValue = inputedText;
-
-                const el = document.getElementById(`divID-${this.idOfcurrentElement}`);
-                console.log(`divID-${this.idOfcurrentElement}`);
-                console.log(this);
-                el.firstElementChild.textContent = inputedText;
-
-                // addTaskButton.removeEventListener()
-            });
-            
-        });
+        this.editButton.addEventListener("click", () => this.#handleEditTask());
     }
 }
 
@@ -85,16 +77,12 @@ function addNewTask() {
         divEl.append(newTaskItem.deleteButton); 
         divEl.append(newTaskItem.editButton); 
         newTaskItem.attachEventListeners();
+        inputElement.value = "";
 
 console.log(newTaskItem);
-
-        inputElement.value = "";
     }else{
         alert("Input is empty. Please enter a task.");
     }
-       
-    
-    
 }
 const addTaskButton = document.getElementById('button-add-task');
 addTaskButton.addEventListener("click", addNewTask);
