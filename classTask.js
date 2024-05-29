4b47c7e7bc5c31bc7ef098aa1c79453895c20da1
nesstio <prykhodko.anastasija@gmail.com>
2024-05-29 06:15:19 +0300

    last working version

---
+++ classTask.js
@@ -0,0 +1,89 @@
+import {lists} from './main.js';
+import {addNewTaskToDom} from './main.js';
+export class Task{
+    inputValue;
+    idOfcurrentElement;
+    
+    deleteButton;
+    editButton;
+
+    static idCount = 0;
+
+    _handleChangedTask;
+    _handleDeleteTask;
+    _handleEditTask;
+
+    constructor(inputValue){
+        this.inputValue = inputValue;
+
+        this.idOfcurrentElement = Task.idCount;
+        Task.idCount += 1;
+
+        this.deleteButton = document.createElement("button");
+        this.deleteButton.textContent = "delete";
+        this.editButton = document.createElement("button"); 
+        this.editButton.textContent = "edit";
+
+        this._handleChangedTask = this.#handleChangedTask.bind(this);
+        this._handleDeleteTask  = this.#handleDeleteTask.bind(this); 
+        this._handleEditTask = this.#handleEditTask.bind(this);
+
+    }
+    
+    #handleChangedTask(){
+
+        const inputElement = document.getElementById("input-text");
+        const inputedText = inputElement.value.trim();
+        const addTaskButton = document.getElementById('button-add-task');
+        this.inputValue = inputedText;
+
+        const el = document.getElementById(`divID-${this.idOfcurrentElement}`);
+        if (el) {
+            el.firstElementChild.textContent = inputedText;
+        }
+
+        addTaskButton.removeEventListener("click", this._handleChangedTask); 
+
+        addTaskButton.addEventListener("click", addNewTaskToDom);
+
+        addTaskButton.style.background = "green";
+        addTaskButton.value = "Add task";
+
+        inputElement.value = "";
+    }
+    #handleDeleteTask(){
+        document.getElementById(`divID-${this.idOfcurrentElement}`).remove();
+        console.log("remove");
+        const currentNameOfList = document.getElementById("place").textContent;
+        const currentList = lists[currentNameOfList]
+        console.log(currentList)
+
+        //! delete item from array of Tasks 
+        currentList.forEach((currentValue, index, array) => {
+            if (currentValue.idOfcurrentElement === this.idOfcurrentElement) {
+                array.splice(index, 1);
+            }
+        });
+        console.log(currentList)        
+    }
+    #handleEditTask(){
+        document.getElementById("input-text").value = this.inputValue;
+        const addTaskButton = document.getElementById('button-add-task');   
+        addTaskButton.removeEventListener("click", addNewTaskToDom);
+        addTaskButton.style.background = "yellow";
+        addTaskButton.value = "change";
+
+        //! проверка на одинаковость ссылок
+        const handler1 = this._handleChangedTask;
+        const handler2 = this._handleChangedTask;
+        console.log(handler1 === handler2); // true
+
+        // !!УДАЛЕНИЕ ОБРАБОТЧИКА СОБЫТИЙ НА ВСЯКИЙ СЛУЧАЙ
+        addTaskButton.removeEventListener("click", handler1); //!!
+        addTaskButton.addEventListener("click", handler2); 
+    }
+    attachEventListeners(){
+        this.deleteButton.addEventListener("click",  this._handleDeleteTask);
+        this.editButton.addEventListener("click", this._handleEditTask);
+    }
+}
--- main.js
+++ main.js
@@ -1,97 +1,10 @@
-class Task{
-    inputValue;
-    idOfcurrentElement;
-    
-    deleteButton;
-    editButton;
+import {Task} from './classTask.js';
 
-    static idCount = 0;
-
-    _handleChangedTask;
-    _handleDeleteTask;
-    _handleEditTask;
-
-    constructor(inputValue){
-        this.inputValue = inputValue;
-
-        this.idOfcurrentElement = Task.idCount;
-        Task.idCount += 1;
-
-        this.deleteButton = document.createElement("button");
-        this.deleteButton.textContent = "delete";
-        this.editButton = document.createElement("button"); 
-        this.editButton.textContent = "edit";
-
-        this._handleChangedTask = this.#handleChangedTask.bind(this);
-        this._handleDeleteTask  = this.#handleDeleteTask.bind(this); 
-        this._handleEditTask = this.#handleEditTask.bind(this);
-
-    }
-    
-    #handleChangedTask(){
-        
-
-        const inputElement = document.getElementById("input-text");
-        const inputedText = inputElement.value.trim();
-        const addTaskButton = document.getElementById('button-add-task');
-        // addTaskButton.removeEventListener("click", this._handleChangedTask); //!
-        this.inputValue = inputedText;
-
-        const el = document.getElementById(`divID-${this.idOfcurrentElement}`);
-        if (el) {
-            el.firstElementChild.textContent = inputedText;
-        }
-
-        addTaskButton.removeEventListener("click", this._handleChangedTask); 
-
-        addTaskButton.addEventListener("click", addNewTaskToDom);
-
-        addTaskButton.style.background = "green";
-        addTaskButton.value = "Add task";
-
-        inputElement.value = "";
-    }
-    #handleDeleteTask(){
-        document.getElementById(`divID-${this.idOfcurrentElement}`).remove();
-console.log("remove");
-        const currentNameOfList = document.getElementById("place").textContent;
-        const currentList = lists[currentNameOfList]
-console.log(currentList)
-        //! delete item from array of Tasks 
-        currentList.forEach((currentValue, index, array) => {
-            if (currentValue.idOfcurrentElement === this.idOfcurrentElement) {
-                array.splice(index, 1);
-            }
-        });
-console.log(currentList)        
-    }
-    #handleEditTask(){
-        document.getElementById("input-text").value = this.inputValue;
-        const addTaskButton = document.getElementById('button-add-task');   
-        addTaskButton.removeEventListener("click", addNewTaskToDom);
-        addTaskButton.style.background = "yellow";
-        addTaskButton.value = "change";
-
-        //! проверка на одинаковость ссылок
-        const handler1 = this._handleChangedTask;
-        const handler2 = this._handleChangedTask;
-        console.log(handler1 === handler2); // true
-
-        // !!УДАЛЕНИЕ ОБРАБОТЧИКА СОБЫТИЙ НА ВСЯКИЙ СЛУЧАЙ
-        addTaskButton.removeEventListener("click", handler1); //!!
-        
-        addTaskButton.addEventListener("click", handler2); 
-    }
-    attachEventListeners(){
-        this.deleteButton.addEventListener("click",  this._handleDeleteTask);
-
-        this.editButton.addEventListener("click", this._handleEditTask);
-    }
-}
-
-
+export const lists = {};
+const addNewTodoListButton = document.getElementById("add-new-todo-list");
+addNewTodoListButton.addEventListener("click", addNewTodoListHandler);
 // ! ******************************************** ! //
-function addNewTaskToDom() {
+export function addNewTaskToDom() {
     const inputElement = document.getElementById("input-text");
     const inputValue = inputElement.value.trim();
     const list = document.getElementById("list-of-tasks");
@@ -100,13 +13,14 @@ function addNewTaskToDom() {
         const newTaskItem = new Task(inputValue); 
         const divEl = document.createElement("div");
         divEl.setAttribute("id", `divID-${newTaskItem.idOfcurrentElement}`);
+        divEl.classList.add("task-line");
 
         list.append(divEl);
         const spanText = document.createElement("span");
         divEl.append(spanText);
         spanText.append(newTaskItem.inputValue);
         
-        divEl.append(newTaskItem.idOfcurrentElement);
+        // divEl.append(newTaskItem.idOfcurrentElement);
         divEl.append(newTaskItem.deleteButton); 
         divEl.append(newTaskItem.editButton); 
         newTaskItem.attachEventListeners();
@@ -114,7 +28,7 @@ function addNewTaskToDom() {
 
         addNewTaskToCertainList(newTaskItem); 
 
-console.log(newTaskItem);
+        console.log(newTaskItem);
     }else{
         alert("Input is empty. Please enter a task.");
     }
@@ -124,11 +38,10 @@ function addNewTaskToCertainList(newTaskItem) {
     const selectedListOption = dropdown.querySelector('option:checked');
     const listName = selectedListOption.text;
     lists[listName].push(newTaskItem);
-console.log(lists);
+    console.log(lists);
 
 }
 function addNewTodoListHandler(){
-    
     const listName = document.getElementById("name-of-new-todo-list").value.trim();
 
     if (!listName) {
@@ -156,42 +69,48 @@ function addNewTodoListHandler(){
         dropdown.addEventListener("change", () => {
             const currentNameOfList = document.querySelector('option:checked').text;
             const h2 = document.getElementById("place");
-            const list = document.getElementById("list-of-tasks");
-
             h2.textContent = currentNameOfList;
-console.log(currentNameOfList);
-            list.innerHTML = "";
+
+            if (currentNameOfList !== "choose your list") {
+                document.getElementById("input-block").classList.remove("hide");
+                const list = document.getElementById("list-of-tasks");
+                
+                console.log(currentNameOfList);
+                list.innerHTML = "";
 
             //! вывод всех задач из текущего списка задач
             renderTaskList(currentNameOfList, list);
-            
+            }else{
+                document.getElementById("input-block").classList.add("hide");
+                document.getElementById("list-of-tasks").innerHTML = "";                
+            }
         }); 
-        
-        const addTaskButton = document.getElementById('button-add-task');
-        addTaskButton.addEventListener("click", addNewTaskToDom);        
+            const addTaskButton = document.getElementById('button-add-task');
+            addTaskButton.addEventListener("click", addNewTaskToDom);        
     }
-console.log(lists);
+    console.log(lists);
 }
 function renderTaskList(currentNameOfList, list) {
     const currentListOfTasks = lists[currentNameOfList];
-        for (let i = 0; i < currentListOfTasks.length; i++) {
-            const inputValue = currentListOfTasks[i].inputValue;
-            const newTaskItem = currentListOfTasks[i];
-            if (inputValue) {
-                const divEl = document.createElement("div");
-                divEl.setAttribute("id", `divID-${newTaskItem.idOfcurrentElement}`);
-        
-                list.append(divEl);
-                const spanText = document.createElement("span");
-                divEl.append(spanText);
-                spanText.append(newTaskItem.inputValue);
-                
-                divEl.append(newTaskItem.idOfcurrentElement);
-                divEl.append(newTaskItem.deleteButton); 
-                divEl.append(newTaskItem.editButton); 
-                newTaskItem.attachEventListeners();
-            }
+    for (let i = 0; i < currentListOfTasks.length; i++) {
+        const inputValue = currentListOfTasks[i].inputValue;
+        const newTaskItem = currentListOfTasks[i];
+        if (inputValue) {
+            const divEl = document.createElement("div");
+            divEl.setAttribute("id", `divID-${newTaskItem.idOfcurrentElement}`);
+            divEl.classList.add("task-line");
+    
+            list.append(divEl);
+            const spanText = document.createElement("span");
+            divEl.append(spanText);
+            spanText.append(newTaskItem.inputValue);
+            
+            // divEl.append(newTaskItem.idOfcurrentElement);
+            divEl.append(newTaskItem.deleteButton); 
+            divEl.append(newTaskItem.editButton); 
+            newTaskItem.attachEventListeners();
         }
+    }
 }
 function createTaskInputAndRenderListToDom(listName) {
     const headerOfCertainList = document.createElement("h2");
@@ -224,10 +143,7 @@ function createTaskInputAndRenderListToDom(listName) {
     divInputBlock.appendChild(inputText);
     divInputBlock.appendChild(addTaskButton);
     divInputBlock.after(listOfTasks);
-
 }
 // ! *********************************************!//
-const lists = {};
-const addNewTodoListButton = document.getElementById("add-new-todo-list");
-addNewTodoListButton.addEventListener("click", addNewTodoListHandler); 
+ 
 
